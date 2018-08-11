d6f4f217b5aa6d210b7f543dcf4dc0d5a04860ec
Karl <1632083718@qq.com>
2018-08-11 09:17:42 +0800

    原来树形dp还可以用树点结构体,比边结构体好多了

--- SRC/hduoj1520.cpp
+++ SRC/hduoj1520.cpp
@@ -9,7 +9,7 @@ struct edge{
     int dest;
     struct edge*next;
 }e[maxm],*head[maxn];
-int cnt,w[maxn],pre[maxn],f[maxn],g[maxn];
+int cnt,pre[maxn],f[maxn],g[maxn];
 void addedge(int u,int v){//v->u
     e[cnt].dest=u;
     e[cnt].next=head[v];
@@ -34,17 +34,15 @@ int main(){
     while(~scanf("%d",&n)){
         memset(head,cnt=0,sizeof head);
         memset(pre,0,sizeof pre);
-        memset(f,0,sizeof f);
         memset(g,0,sizeof g);
         for(int i=1;i<=n;i++)
-            scanf("%d",w+i);
+            scanf("%d",f+i);
         int u,v;
         while(scanf("%d%d",&u,&v)&&u+v)
             addedge(u,v);
         int rt=bfs();
         while(bfsid->size()){
             u=bfsid->top();bfsid->pop();
-            f[u]+=w[u];
             v=f[u]>g[u]?f[u]:g[u];
             f[pre[u]]+=g[u];
             g[pre[u]]+=v;
@@ -52,4 +50,67 @@ int main(){
         printf("%d\n",f[rt]>g[rt]?f[rt]:g[rt]);
     }delete bfsid;
     return 0;
+}
+//结构体版
+#include<stdio.h>
+#include<string.h>
+struct TreeNode{
+    int f;//father
+    int b;//brother
+    int c;//child
+    //special attr:
+    int s;//selected dp val
+    int u;//unselect dp val
+    //special func:
+    int max(){return s>u?s:u;}
+}node[6005];
+void dfs_dp(int rt){
+    for(int i=node[rt].c;i;i=node[i].b){
+        dfs_dp(i);
+        node[rt].s+=node[i].u;
+        node[rt].u+=node[i].max();
+    }
+}
+int main(){
+    int n;
+    while(~scanf("%d",&n)){
+        memset(node,0,sizeof node);
+        for(int i=1;i<=n;i++)
+            scanf("%d",&node[i].s);
+        for(int u,v;scanf("%d%d",&u,&v)&&u+v;){
+            node[u].f=v;
+            node[u].b=node[v].c;
+            node[v].c=u;
+        }
+        int rt=1;//root
+        while(node[rt].f)
+            rt=node[rt].f;
+        dfs_dp(rt);
+        printf("%d\n",node[rt].max());
+    }
+}
+//数组版,大概比结构体快,代码量少吧
+#include<stdio.h>
+#include<string.h>
+#define clr(arr) memset(arr,0,sizeof arr)
+typedef int array[6005];
+array f,b,c,s,u;
+int max(int x){return s[x]>u[x]?s[x]:u[x];}
+void dfs_dp(int rt){
+    for(int i=c[rt];i;i=b[i]){
+        dfs_dp(i);
+        s[rt]+=u[i];
+        u[rt]+=max(i);
+    }
+}
+int main(){
+    for(int n;~scanf("%d",&n)){
+        clr(f);clr(b);clr(c);clr(u);
+        for(int i=1;i<=n;i++)scanf("%d",s+i);
+        for(int u,v;scanf("%d%d",&u,&v)&&u+v;)
+            b[u]=c[f[u]=v],c[v]=u;
+        int rt=1;while(f[rt])rt=f[rt];
+        dfs_dp(rt);
+        printf("%d\n",max(rt));
+    }
 }
