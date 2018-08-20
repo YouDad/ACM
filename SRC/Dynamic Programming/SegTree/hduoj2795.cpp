2bfa7246b943e8e019ab8d33f5a8dc8bfddd874b
Karl <1632083718@qq.com>
2018-08-20 11:05:16 +0800

    发现更好的查询方法 此题为维护区间最大值,操作有单点减少和区间符合要求最小下标.

--- SRC/Dynamic Programming/SegTree/hduoj2795.cpp
+++ SRC/Dynamic Programming/SegTree/hduoj2795.cpp
@@ -1,27 +1,17 @@
 #include<stdio.h>
-inline int max(int a,int b){return a>b?a:b;}
+#include<algorithm>
 int arr[1<<19];
 int main(){
-    for(int h,w,n;~scanf("%d%d%d",&h,&w,&n);){
-        int M=1;while(M-2<n)M<<=1;
-        for(int i=1;i<=M+n;i++)arr[i]=w;
-        for(int i=0,q;i<n;i++){
-            scanf("%d",&q);
-            int l=1,r=n,p=M+h+1;
-            for(l+=M-1,r+=M+1;l^r^1;l>>=1,r>>=1){
-                if(~l&1)if(arr[l^1]>=q){
-                    w=l^1;while(w<M)w=w*2+(arr[w<<1]<q);
-                    if(p>w)p=w;
-                }
-                if( r&1)if(arr[r^1]>=q){
-                    w=r^1;while(w<M)w=w*2+(arr[w<<1]<q);
-                    if(p>w)p=w;
-                }
-            }
-            if(p-M<=h){
+    for(int h,w,m;~scanf("%d%d%d",&h,&w,&m);){
+        int n=m<h?m:h,M=1;while(M-2<n)M<<=1;
+        for(int i=1;i<=M+n;i++)arr[i]=w;arr[M]=0;
+        while(m--){
+            int q,p=1;scanf("%d",&q);
+            while(p<M)p=p*2+(arr[p<<1]<q);
+            if(arr[p]>=q&&p-M<=n){
                 printf("%d\n",p-M);
                 for(arr[p]-=q,p>>=1;p;p>>=1)
-                    arr[p]=max(arr[p<<1],arr[p<<1|1]);
+                    arr[p]=std::max(arr[p<<1],arr[p<<1|1]);
             }else puts("-1");
         }
     }
