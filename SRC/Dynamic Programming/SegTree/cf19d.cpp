0998723cb1574a4cd24634db9192ebfc158662b7
Karl <1632083718@qq.com>
2018-08-21 17:30:07 +0800

    有一些bug修复一下

--- SRC/Dynamic Programming/SegTree/cf19d.cpp
+++ SRC/Dynamic Programming/SegTree/cf19d.cpp
@@ -3,43 +3,44 @@
 #include<string.h>
 #include<algorithm>
 using namespace std;
-set<int,greater<int> >s[200005];char op[200005];
-int x[200005],y[200005],sorted[200005],arr[1<<19];
+set<int,greater<int> >s[200005];
+int arr[1<<19],x[200005],y[200005];
+char op[200005];
+int sorted[200005];
 int main(){
     int n;scanf("%d%*c",&n);
     for(int i=0;i<n;i++)scanf("%c%*s %d %d%*c",op+i,x+i,y+i);
     memcpy(sorted,x,n*4);sort(sorted,sorted+n);
-    int sortlen=unique(sorted,sorted+n)-sorted;
-    int M=1;while(M-2<sortlen)M<<=1;
+    int*sorted_end=unique(sorted,sorted+n);
+    int sortlen=sorted_end-sorted;
+    int M=1;while(M-2<sortlen)M*=2;
     for(int i=0;i<n;i++){
-        int pos=lower_bound(sorted,sorted+sortlen,x[i])-sorted;
+        int p=lower_bound(sorted,sorted_end,x[i])-sorted;
         if(op[i]=='f'){
-            int l=pos+1,r=sortlen,mpos=sortlen+M+1;//minpos
-            for(l+=M-1,r+=M+1;l^r^1;l>>=1,r>>=1){
+            int l=p+1,r=sortlen,mp=r+M+1;//minpos
+            for(l+=M-1,r+=M+1;l^r^1;l/=2,r/=2){
                 if((~l&1)&&arr[l^1]>y[i])
-                    for(int p=l^1;p<M||(mpos=min(mpos,p));)
-                        p=p*2+(arr[p<<1]<=y[i]);
+                    for(int x=l^1;x<M||(mp=min(mp,x))&&false;)
+                        x=x*2+(arr[x*2]<=y[i]);
                 if(( r&1)&&arr[r^1]>y[i])
-                    for(int p=r^1;p<M||(mpos=min(mpos,p));)
-                        p=p*2+(arr[p<<1]<=y[i]);
+                    for(int x=r^1;x<M||(mp=min(mp,x))&&false;)
+                        x=x*2+(arr[x*2]<=y[i]);
             }
-            if(mpos-M>sortlen)puts("-1");
-            else
-                for(auto it=s[mpos-=M].crbegin();it!=s[mpos].crend();it++)
-                    if(*it>y[i]){printf("%d %d\n",sorted[mpos],*it);break;}
-        }
-        else{
+            if(mp-M>sortlen)puts("-1");
+            else{mp-=M;
+                for(auto it=s[mp].crbegin();it!=s[mp].crend();it++)
+                    if(*it>y[i]){printf("%d %d\n",sorted[mp],*it);break;}
+            }
+        }else{
             if(op[i]=='a'){
-                s[pos].emplace(y[i]);
-                arr[pos+M]=max(arr[pos+M],y[i]);
-            }else if(op[i]=='r'){
-                s[pos].erase(s[pos].begin());
-                arr[pos+M]=0;
-                if(!s[pos].empty())
-                    arr[pos+M]=*s[pos].cbegin();
+                s[p].emplace(y[i]);
+                arr[p+M]=max(arr[p+M],y[i]);
+            }else{
+                for(auto it=s[p].begin();it!=s[p].end();it++)
+                    if(*it==y[i]){s[p].erase(it);break;}
+                arr[p+M]=0;if(!s[p].empty())arr[p+M]=*s[p].cbegin();
             }
-            for(pos=pos+M>>1;pos;pos>>=1)
-                arr[pos]=max(arr[pos<<1],arr[pos<<1|1]);
+            for(p=p+M>>1;p;p/=2)arr[p]=max(arr[p*2],arr[p*2+1]);
         }
     }
     return 0;
