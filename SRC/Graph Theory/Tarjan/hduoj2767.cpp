810cdafd09e16f15620705c424533fbe3b06c706
Karl <1632083718@qq.com>
2018-09-12 19:18:55 +0800

    9.12连通图(1/1)(至少加几条边全图强连通)+更改tarjan板子

--- SRC/Graph Theory/Tarjan/hduoj2767.cpp
+++ SRC/Graph Theory/Tarjan/hduoj2767.cpp
@@ -2,11 +2,11 @@
 #include<string.h>
 #include<stack>
 inline int min(int a,int b){return a<b?a:b;}
-const int maxn=10005;
+const int maxn=20005;
 struct edge{
     int dest;
     struct edge*next;
-}e[100005],*head[maxn];
+}e[50005],*head[maxn];
 int cnt,id,bcnt;
 int dfsid[maxn],minid[maxn],belong[maxn];
 bool ins[maxn];
@@ -35,16 +35,40 @@ void tarjan(int u){
         }s->pop();
     }
 }
+int in[maxn],out[maxn];
 int main(){
+#ifdef LOCAL_DEBUG
+    freopen("E:/ACM/SRC/1.txt","r",stdin);
+#endif
     s=new std::stack<int>();
-    for(int n,m;scanf("%d%d",&n,&m)&&n+m;){
+    int t;scanf("%d",&t);
+    while(t--){
+        int n,m,u,v;scanf("%d%d",&n,&m);
         memset(head,cnt=0,sizeof head);
         memset(dfsid,bcnt=id=0,sizeof dfsid);
-        for(int u,v;m--;addedge(u,v))
+        memset(in,0,sizeof in);
+        memset(out,0,sizeof out);
+        while(m--){
             scanf("%d%d",&u,&v);
+            addedge(u,v);
+        }
         for(int i=1;i<=n;i++)
-            if(dfsid[i]==0)
+            if(!dfsid[i])
                 tarjan(i);
-        puts(bcnt-1?"No":"Yes");
+        if(bcnt==1){
+            puts("0");
+            continue;
+        }
+        for(int u=1;u<=n;u++)
+            for(edge*i=head[u];i;i=i->next)
+                if(belong[u]!=belong[i->dest])
+                    in[belong[i->dest]]=1,out[belong[u]]=1;
+        int icnt=bcnt,ocnt=bcnt;
+        for(int i=1;i<=bcnt;i++){
+            icnt-=in[i];
+            ocnt-=out[i];
+        }
+        printf("%d\n",icnt>ocnt?icnt:ocnt);
     }
+    return 0;
 }
--- TMP/graphs/tarjan.cpp
+++ TMP/graphs/tarjan.cpp
@@ -3,23 +3,23 @@
 inline int min(int a,int b){return a<b?a:b;}
 const int maxn=10005;
 struct edge{
-    int to;
+    int dest;
     struct edge*next;
 }e[100005],*head[maxn];
-int cnt,id;
+int cnt,id,bcnt;
 int dfsid[maxn],minid[maxn],belong[maxn];
 bool ins[maxn];
 void addedge(int u,int v){
-    e[cnt].to=v;
+    e[cnt].dest=v;
     e[cnt].next=head[u];
     head[u]=e+cnt++;
 }
-std::stack<int>*s;//别忘了new std::stack<in
+std::stack<int>*s;
 void tarjan(int u){
     dfsid[u]=minid[u]=++id;
     s->push(u);ins[u]=true;
     for(edge*i=head[u];i;i=i->next){
-        int v=i->to;
+        int v=i->dest;
         if(dfsid[v]==0)
             tarjan(v),
             minid[u]=min(minid[u],minid[v]);
@@ -27,9 +27,14 @@ void tarjan(int u){
             minid[u]=min(minid[u],dfsid[v]);
     }
     if(dfsid[u]==minid[u]){
-        for(;s->top()!=u;s->pop()){
+        for(bcnt++;true;s->pop()){
             ins[s->top()]=false;
-            belong[s->top()]=ans;
+            belong[s->top()]=bcnt;
+            if(s->top()==u)break;
         }s->pop();
     }
 }
+//s=new std::stack<int>();
+//memset(dfsid,bcnt=id=0,sizeof dfsid);
+//memset(head,cnt=0,sizeof head);
+//if dfsid == 0 meaning this isn't be visited
