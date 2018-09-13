5c236d53f4fd9f5851530a2c32d974551b3b200a
Karl <1632083718@qq.com>
2018-09-13 19:33:18 +0800

    9.13二分匹配(1/1)(二分图+最大匹配+hk算法,距离符合一定要求建边)+hk板子

---
+++ SRC/Graph Theory/二分匹配/hduoj2389.cpp
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<string.h>
+#include<queue>
+const int maxn=3005,inf=0x3f3f3f3f;
+struct edge{
+    int dest;
+    edge*next;
+}e[maxn*maxn],*head[maxn];
+int cnt,nx,ny;
+int mx[maxn],my[maxn];
+int dx[maxn],dy[maxn],dist;
+bool vis[maxn];
+void addedge(int u,int v){
+    e[cnt].dest=v;
+    e[cnt].next=head[u];
+    head[u]=e+cnt++;
+}
+bool aug(){
+    std::queue<int>q;
+    dist=inf;
+    memset(dx,-1,sizeof dx);
+    memset(dy,-1,sizeof dy);
+    for(int i=0;i<nx;i++)
+        if(mx[i]==-1)
+            q.push(i),dx[i]=0;
+    while(q.size()){
+        int u=q.front();q.pop();
+        if(dx[u]>dist)break;
+        for(edge*i=head[u];i;i=i->next){
+            int v=i->dest;
+            if(~dy[v])continue;
+            dy[v]=dx[u]+1;
+            if(~my[v]){
+                dx[my[v]]=dy[v]+1;
+                q.push(my[v]);
+            }else dist=dy[v];
+        }
+    }
+    return dist!=inf;
+}
+bool dfs(int u){
+    for(edge*i=head[u];i;i=i->next){
+        int v=i->dest;
+        if(!vis[v]&&dy[v]==dx[u]+1){
+            vis[v]=true;
+            if(~my[v]&&dy[v]==dist)
+                continue;
+            if(my[v]==-1||dfs(my[v])){
+                my[v]=u;
+                mx[u]=v;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int hk(){
+    int ret=0;
+    memset(mx,-1,sizeof mx);
+    memset(my,-1,sizeof my);
+    while(aug()){
+        memset(vis,false,sizeof vis);
+        for(int i=0;i<nx;i++)
+            if(!~mx[i]&&dfs(i))
+                ret++;
+    }
+    return ret;
+}
+#include<math.h>
+int abs(int x){return x<0?-x:x;}
+bool distance(int x,int y,int v,int ux,int uy,int t){
+    return v*t*v*t>=(ux-x)*(ux-x)+(uy-y)*(uy-y);
+}
+int x[maxn],y[maxn],v[maxn],ubx[maxn],uby[maxn];
+int main(){
+#ifdef LOCAL_DEBUG
+    freopen("E:/ACM/SRC/1.txt","r",stdin);
+#endif
+    int t;scanf("%d",&t);
+    for(int kase=1;t--;kase++){
+        printf("Scenario #%d:\n",kase);
+        memset(head,cnt=0,sizeof head);
+        int t;scanf("%d%d",&t,&nx);
+        for(int i=0;i<nx;i++)
+            scanf("%d%d%d",x+i,y+i,v+i);
+        scanf("%d",&ny);
+        for(int i=0;i<ny;i++)
+            scanf("%d%d",ubx+i,uby+i);
+        for(int i=0;i<nx;i++)
+            for(int j=0;j<ny;j++)
+                if(distance(x[i],y[i],v[i],ubx[j],uby[j],t))
+                    addedge(i,j);
+        printf("%d\n\n",hk());
+    }
+    return 0;
+}
