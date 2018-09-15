a44b58088725ee2909715798f3526263919289c8
Karl <1632083718@qq.com>
2018-09-15 12:37:25 +0800

    G是费马小定理+快速幂

---
+++ SRC/2018ICPC/Web_JiaoZuo/G.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+typedef long long ll;
+const int mod=1e9+7;
+ll pow(ll x,ll y){
+    ll ret=1;y%=mod;
+    while(y){
+        if(y&1)ret=ret*x%mod;
+        x=x*x%mod;y/=2;
+    }
+    return ret;
+}
+int main(){
+#ifdef LOCAL_DEBUG
+    freopen("E:/ACM/SRC/1.txt","r",stdin);
+#endif
+    int t;scanf("%d%*c",&t);
+    while(t--){
+        ll y=0;char c;
+        while((c=getchar())!='\n'&&c!=EOF)
+            y=(y*10+c-'0')%(mod-1);
+        printf("%lld\n",pow(2,y-1+mod));
+    }
+    return 0;
+}
